49e344209f8c32a11fe14f278ca15f7a0b058f9d
gulyapulya <52351598+gulyapulya@users.noreply.github.com>
2020-04-16 18:27:48 -0400

    final

---
+++ js/script.js
@@ -0,0 +1,49 @@
+function clear() {
+  var inputs = document.querySelectorAll('input');
+  inputs.innerHMTL = '';
+}
+
+function orderPrCheck() {
+  if (document.getElementById('purpose-3').checked)
+    document.getElementById('reveal-if-checked').style.display = 'block';
+  else document.getElementById('reveal-if-checked').style.display = 'none';
+}
+
+function showRoses() {
+  var rosesDivs = document.getElementsByClassName('roses');
+  for (var i = 0; i < rosesDivs.length; i++) {
+    rosesDivs[i].style.display = 'block';
+  }
+  var bouquetsDivs = document.getElementsByClassName('bouquets');
+  for (var j = 0; j < bouquetsDivs.length; j++) {
+    bouquetsDivs[j].style.display = 'none';
+  }
+}
+
+function showBouquets() {
+  var rosesDivs = document.getElementsByClassName('roses');
+  for (var i = 0; i < rosesDivs.length; i++) {
+    rosesDivs[i].style.display = 'none';
+  }
+  var bouquetsDivs = document.getElementsByClassName('bouquets');
+  for (var k = 0; k < bouquetsDivs.length; k++) {
+    bouquetsDivs[k].style.display = 'block';
+  }
+}
+
+function showAll() {
+  var rosesDivs = document.getElementsByClassName('roses');
+  for (var i = 0; i < rosesDivs.length; i++) {
+    rosesDivs[i].style.display = 'block';
+  }
+  var bouquetsDivs = document.getElementsByClassName('bouquets');
+  for (var l = 0; l < bouquetsDivs.length; l++) {
+    bouquetsDivs[l].style.display = 'block';
+  }
+}
+
+clear();
+orderPrCheck();
+showRoses();
+showBouquets();
+showAll();
