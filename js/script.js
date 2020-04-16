1c4068200a42c56d7794ab381dbd3b4bc510a85a
gulyapulya <52351598+gulyapulya@users.noreply.github.com>
2020-04-16 19:32:56 -0400

    edited

--- js/script.js
+++ js/script.js
@@ -1,44 +1,44 @@
 function clear() {
-  var inputs = document.querySelectorAll('input');
-  inputs.innerHMTL = '';
+  var inputs = document.querySelectorAll("input");
+  inputs.innerHMTL = "";
 }
 
 function orderPrCheck() {
-  if (document.getElementById('purpose-3').checked)
-    document.getElementById('reveal-if-checked').style.display = 'block';
-  else document.getElementById('reveal-if-checked').style.display = 'none';
+  if (document.getElementById("purpose-3").checked)
+    document.getElementById("reveal-if-checked").style.display = "block";
+  else document.getElementById("reveal-if-checked").style.display = "none";
 }
 
 function showRoses() {
-  var rosesDivs = document.getElementsByClassName('roses');
+  var rosesDivs = document.getElementsByClassName("roses");
   for (var i = 0; i < rosesDivs.length; i++) {
-    rosesDivs[i].style.display = 'block';
+    rosesDivs[i].style.display = "block";
   }
-  var bouquetsDivs = document.getElementsByClassName('bouquets');
+  var bouquetsDivs = document.getElementsByClassName("bouquets");
   for (var j = 0; j < bouquetsDivs.length; j++) {
-    bouquetsDivs[j].style.display = 'none';
+    bouquetsDivs[j].style.display = "none";
   }
 }
 
 function showBouquets() {
-  var rosesDivs = document.getElementsByClassName('roses');
+  var rosesDivs = document.getElementsByClassName("roses");
   for (var i = 0; i < rosesDivs.length; i++) {
-    rosesDivs[i].style.display = 'none';
+    rosesDivs[i].style.display = "none";
   }
-  var bouquetsDivs = document.getElementsByClassName('bouquets');
+  var bouquetsDivs = document.getElementsByClassName("bouquets");
   for (var k = 0; k < bouquetsDivs.length; k++) {
-    bouquetsDivs[k].style.display = 'block';
+    bouquetsDivs[k].style.display = "block";
   }
 }
 
 function showAll() {
-  var rosesDivs = document.getElementsByClassName('roses');
+  var rosesDivs = document.getElementsByClassName("roses");
   for (var i = 0; i < rosesDivs.length; i++) {
-    rosesDivs[i].style.display = 'block';
+    rosesDivs[i].style.display = "block";
   }
-  var bouquetsDivs = document.getElementsByClassName('bouquets');
+  var bouquetsDivs = document.getElementsByClassName("bouquets");
   for (var l = 0; l < bouquetsDivs.length; l++) {
-    bouquetsDivs[l].style.display = 'block';
+    bouquetsDivs[l].style.display = "block";
   }
 }
 
